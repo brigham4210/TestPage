3f88f69477cac4d43ea632fb2bc20e27a6123599
Brigham Chen <83602138+brigham4210@users.noreply.github.com>
2023-11-14 16:36:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const rate = document.getElementById('starRating');
   const commentBox = document.getElementById('commentBox');
 
-  // Hide the comment box initially
+  // Hide the comment box and stars initially
   commentBox.style.display = 'none';
+  rate.style.display = 'none';
 
   feedbackBox.addEventListener('mouseenter', function () {
     rate.style.display = 'block';
@@ -19,5 +20,20 @@ document.addEventListener('DOMContentLoaded', function () {
   rate.addEventListener('change', function () {
     // Display the comment box when a star is clicked
     commentBox.style.display = 'block';
+
+    // Hide the stars after a star is clicked
+    rate.style.display = 'none';
+  });
+
+  rate.addEventListener('mouseenter', function () {
+    // Show the stars when the user hovers over the rating area
+    rate.style.display = 'block';
+  });
+
+  rate.addEventListener('mouseleave', function () {
+    if (!rate.contains(document.activeElement)) {
+      // Hide the stars when the user leaves the rating area, but not if a star is clicked
+      rate.style.display = 'none';
+    }
   });
 });
