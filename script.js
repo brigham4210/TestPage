3930f5767b2109536614cd37356616abf5413531
Brigham Chen <83602138+brigham4210@users.noreply.github.com>
2023-11-14 16:21:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const feedbackBox = document.querySelector('.feedback-box');
   const rate = document.getElementById('starRating');
   const commentBox = document.getElementById('commentBox');
-
-  feedbackBox.addEventListener('mouseenter', function () {
-    rate.style.display = 'block';
-    commentBox.style.display = 'block';
-  });
-
-  feedbackBox.addEventListener('mouseleave', function () {
-    rate.style.display = 'none';
-    commentBox.style.display = 'none';
-  });
 
   rate.addEventListener('change', function () {
     commentBox.style.display = 'block';
